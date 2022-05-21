1424ad6feae5905a3710143b7bbb32e3b4fe314e
itharigen <itharigen@aracnosoft.es>
2022-05-21 12:59:01 +0200

    css and text modified

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-const permission = document.querySelector('.permission');
-const positive = document.querySelector('.positive');
-const negative = document.querySelector('.negative');
-const error = document.querySelector('.error');
+const permission = document.querySelector(".permission");
+const positive = document.querySelector(".positive");
+const negative = document.querySelector(".negative");
+const error = document.querySelector(".error");
 
-const API_KEY = '9dc488e97d8239b0fabeec683e6d440b';
+const API_KEY = "9dc488e97d8239b0fabeec683e6d440b";
 const RAIN_MARGIN = 8;
 
 // ======================================================================
@@ -14,7 +14,7 @@ async function getData({ url, options = {} }) {
   const response = await fetch(url, options);
 
   if (!response.ok) {
-    throw new Error('Error while trying to fetch');
+    throw new Error("Error while trying to fetch");
   }
 
   const data = await response.json();
@@ -23,33 +23,29 @@ async function getData({ url, options = {} }) {
 
 function showPanel(panel) {
   hideAllPanels();
-  panel.classList.remove('hidden');
+  panel.classList.remove("hidden");
 }
 
 function hideAllPanels() {
-  permission.classList.add('hidden');
-  positive.classList.add('hidden');
-  negative.classList.add('hidden');
-  error.classList.add('hidden');
+  permission.classList.add("hidden");
+  positive.classList.add("hidden");
+  negative.classList.add("hidden");
+  error.classList.add("hidden");
 }
 
 function showPositive({ city, temperature, weather, nextRain }) {
   showPanel(positive);
-  const text = positive.querySelector('p');
+  const text = positive.querySelector("p");
   text.innerHTML = `
-  Ahora mismo hay ${Math.round(temperature)}º C en ${city}, ${weather} y ${
-    nextRain > 0
-      ? `<strong>probablemente llueva en ${nextRain} hora(s)</strong>.`
-      : '<strong>toca agarrar el paraguas</strong>.'
+  ${Math.round(temperature)}º C en ${city}, ${weather}. ${
+    nextRain > 0 ? `<strong>Probablemente llueva en ${nextRain} hora(s)</strong>.` : "<strong>Esta lloviendo</strong>."
   }`;
 }
 function showNegative({ city, temperature, weather }) {
   showPanel(negative);
-  const text = negative.querySelector('p');
+  const text = negative.querySelector("p");
   text.innerHTML = `
-  Ahora mismo hay ${Math.round(
-    temperature
-  )}º C en ${city}, ${weather} y parece que <strong>no lloverá</strong> en las próximas ${RAIN_MARGIN} horas.`;
+  ${Math.round(temperature)}º C en ${city}, ${weather}. <strong>No va a llover</strong> en las próximas ${RAIN_MARGIN} horas.`;
 }
 
 async function getWeatherData(latitude, longitude) {
@@ -57,19 +53,19 @@ async function getWeatherData(latitude, longitude) {
     console.log(latitude, longitude);
     // pedir estado actual a la API
     const currentWeather = await getData({
-      url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=es`
+      url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=es`,
     });
     console.log({ currentWeather });
 
     // pedir prediccion proximas horas a la API
     const nextHours = await getData({
-      url: `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,daily&appid=${API_KEY}&units=metric&lang=es`
+      url: `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,daily&appid=${API_KEY}&units=metric&lang=es`,
     });
     console.log({ nextHours });
 
     // comprobar si va a llover en las proximas RAIN_MARGIN horas
     const nextRain = nextHours.hourly.findIndex((hour) => {
-      return hour.weather[0].main === 'Rain';
+      return hour.weather[0].main === "Rain";
     });
 
     // mostral panel correspondiente si llueve o no lllueve
@@ -79,7 +75,7 @@ async function getWeatherData(latitude, longitude) {
         city: currentWeather.name,
         temperature: currentWeather.main.temp,
         weather: currentWeather.weather[0].description,
-        nextRain
+        nextRain,
       });
     } else {
       // console.log('NO VA A LLOVER');
@@ -87,7 +83,7 @@ async function getWeatherData(latitude, longitude) {
         city: currentWeather.name,
         temperature: currentWeather.main.temp,
         weather: currentWeather.weather[0].description,
-        nextRain
+        nextRain,
       });
     }
   } catch (e) {
@@ -99,28 +95,28 @@ async function getWeatherData(latitude, longitude) {
 function getUserLocation() {
   hideAllPanels();
   // console.log('Asking for location...');
-  if (!localStorage.getItem('position')) {
+  if (!localStorage.getItem("position")) {
     navigator.geolocation.getCurrentPosition(
       (location) => {
         const { latitude, longitude } = location.coords;
         getWeatherData(latitude, longitude);
-        localStorage.setItem('permission', 'ok');
+        localStorage.setItem("permission", "ok");
 
         const position = {
           lat: latitude,
-          long: longitude
+          long: longitude,
         };
 
-        localStorage.setItem('position', JSON.stringify(position));
+        localStorage.setItem("position", JSON.stringify(position));
       },
 
       () => {
-        console.error('error getting location');
+        console.error("error getting location");
         showPanel(error);
       }
     );
   } else {
-    const position = localStorage.getItem('position');
+    const position = localStorage.getItem("position");
     const positionParsed = JSON.parse(position);
     const { lat, long } = positionParsed;
     getWeatherData(lat, long);
@@ -129,17 +125,17 @@ function getUserLocation() {
 
 function main() {
   // REFACTORIZAR PARA NO REPETIR
-  const updateLocationButton = document.querySelectorAll('.update');
+  const updateLocationButton = document.querySelectorAll(".update");
   updateLocationButton.forEach((e) => {
     e.onclick = () => {
-      localStorage.setItem('position', '');
+      localStorage.setItem("position", "");
       getUserLocation();
     };
   });
 
   // Tiempo en ARACNOSOFT
 
-  const aracnobutton = document.querySelectorAll('.aracnobutton');
+  const aracnobutton = document.querySelectorAll(".aracnobutton");
   aracnobutton.forEach((e) => {
     e.onclick = () => {
       getWeatherData(43.306638, -8.2680563);
@@ -148,7 +144,7 @@ function main() {
 
   // Tiempo en ARACNOSOFT
 
-  const sadabutton = document.querySelectorAll('.sadabutton');
+  const sadabutton = document.querySelectorAll(".sadabutton");
   sadabutton.forEach((e) => {
     e.onclick = () => {
       getWeatherData(43.3558231, -8.2615396);
@@ -157,11 +153,11 @@ function main() {
 
   // ======================================
 
-  if (localStorage.getItem('permission') === 'ok') {
+  if (localStorage.getItem("permission") === "ok") {
     getUserLocation();
   } else {
     showPanel(permission);
-    const permissionButton = permission.querySelector('button');
+    const permissionButton = permission.querySelector("button");
     permissionButton.onclick = () => {
       getUserLocation();
     };
